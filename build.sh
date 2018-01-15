dcdc7888311c22081e40bded6700fd25a7cc187e
Emilio Rojas <emilio93@gmail.com>
2018-01-14 19:31:49 -0600

    update tarea1

--- build.sh
+++ build.sh
@@ -1,16 +1,41 @@
+#!/bin/bash
+
+ARG1=${1:-foo}
+
+cwd="$(pwd)"
+
+echo "mkdir -p build"
 mkdir -p build
-for i in tarea1
-do
-    cd trabajo/${i}
-    for k in ej1
-    do 
-        matlab -nodisplay -nodesktop -r "run ${k}.m" 
-    done
-    cd ../..
-    for j in 1 2
-    do
-        pdflatex -interaction=nonstopmode \
+for tarea in ./trabajo/*.tex; do
+
+    if [ $ARG1 == "-matlab" ]; then
+        echo "cd ${tarea/\.tex/}"
+        cd ${tarea/\.tex/}
+        
+        for filename in ./*.m; do
+            echo "matlab -nodisplay -nodesktop \\"
+            echo "-r \"run ${filename}\""
+            matlab -nodisplay -nodesktop \
+            -r "run ${filename}" 
+        done
+        
+        echo "cd ${cwd}"
+        cd ${cwd}
+    fi
+
+    for j in 1 2; do
+        echo ""
+        echo "#############################################################################################"
+        echo ""
+        echo "pdflatex -shell-escape -interaction=nonstopmode \\"
+        echo "-jobname=Tarea1 -output-directory=build \\"
+        echo "-8bit ${tarea} \\"
+        echo "| grep -E \"l\.[0-9]*\s+|!  ==>|!\ LaTeX\ Error|!\ LaTeX\ Warning|Package|Output written|Transcript written\" "
+        echo ""
+        pdflatex -shell-escape -interaction=nonstopmode \
         -jobname=Tarea1 -output-directory=build \
-        -8bit trabajo/${i}.tex
+        -8bit ${tarea} \
+        | grep -E "l\.[0-9]*\s+|!  ==>|!\ LaTeX\ Error|!\ LaTeX\ Warning|Package|Output written|Transcript written" 
     done
+
 done
--- trabajo/tarea1/ej1.m
+++ trabajo/tarea1/ej1.m
@@ -5,52 +5,112 @@
 % Emilio Javier Rojas Álvarez
 
 A=5;
-g=9.81;
-Hmin=2.5;
-Hmax=3.5;
-Htyp=3;
-Kvs=0.001;
-Xvsmin=0.4;
-Xvstyp=0.5;
-Xvsmax=0.6;
-p=1475;
 
-% Vector qe
-rangemin=0;%0.07;
-rangemax=0.14;
-steps=10000;
-qe=rangemin:(rangemax-rangemin)/steps:rangemax;
+Hmin=2.5;   Htyp=3;     Hmax=3.5;
+Xvsmin=0.4; Xvstyp=0.5; Xvsmax=0.6;
 
-y1=(qe/(Xvsmin*Kvs)).^2/(p*g);
-y2=(qe/(Xvstyp*Kvs)).^2/(p*g);
-y3=(qe/(Xvsmax*Kvs)).^2/(p*g);
+% Caracteristicas estáticas segun distorsión Xvs
+y1=qe_h(Xvsmin);
+y2=qe_h(Xvstyp);
+y3=qe_h(Xvsmax);
 
-ymin=Hmin+0*qe;
-ytyp=Htyp+0*qe;
-ymax=Hmax+0*qe;
+% Valores de nivel analizados
+ymin=Hmin+0*qe_vec();
+ytyp=Htyp+0*qe_vec();
+ymax=Hmax+0*qe_vec();
 
-% h=(qe/{xvs*kvs})^2/(pg)
-% =>
-% qe=xvs*kvs*sqrt(p*g*h)
-Qemin=Xvsmin*Kvs*sqrt(p*g*Hmin)
-Qemax=Xvsmax*Kvs*sqrt(p*g*Hmax)
+% Valores de cuadales minimos, tipico y máximo según
+% la distorsión Xvs y nivel H
+Qemin=qe_h_val(Xvsmin,Hmin)
+Qetyp=qe_h_val(Xvstyp,Htyp)
+Qemax=qe_h_val(Xvsmax,Hmax)
 
 figure
-plot(qe,y1,qe,y2,qe,y3,qe,ymin,qe,ymax,qe,ytyp)
 
+plot(qe_vec(),y1,qe_vec(),y2,qe_vec(),y3,qe_vec(),ymin,qe_vec(),ytyp,qe_vec(),ymax)
+title('Característica Estática')
+ylabel('Nivel [m]')
+xlabel('Caudal de Entrada [m^{3}/s]')
+legend('h_{X_{VS}min}(q_e)','h_{X_{VS}typ}(q_e)', 'h_{X_{VS}max}(q_e)','Location','northeastoutside','Orientation','vertical')
 
 ylim([2.4,3.6])
 xlim([0.07,0.14])
-title('Característica Estática')
-ylabel('Nivel')
-xlabel('Caudal de Entrada')
-legend('y_{x_{vs}min}=','y_{x_{vs}typ}', 'y_{x_{vs}max}','Location','northwest','Orientation','horizontal')
 saveas(gcf,'ej1-caracteristicaestaticazoom','epsc')
 
 ylim([0,3.6])
 xlim([0,0.14])
-title('Característica Estática')
-ylabel('Nivel')
-xlabel('Caudal de Entrada')
 saveas(gcf,'ej1-caracteristicaestatica','epsc')
+
+
+%
+% Bloque de derivadas
+%
+
+y1der=qe_der(Xvsmin);
+y2der=qe_der(Xvstyp);
+y3der=qe_der(Xvsmax);
+
+ydermin=qe_der_val(Xvsmin,Qemax)
+ydermin=qe_der_val(Xvsmin,Qemax)+0*qe_vec();
+
+ydertyp=qe_der_val(Xvstyp,Qetyp)
+ydertyp=qe_der_val(Xvstyp,Qetyp)+0*qe_vec();
+
+ydermax=qe_der_val(Xvsmax,Qemin)
+ydermax=qe_der_val(Xvsmax,Qemin)+0*qe_vec();
+
+figure
+
+plot(qe_vec(),y1der,qe_vec(),y2der,qe_vec(),y3der,qe_vec(),ydermin,qe_vec(),ydertyp,qe_vec(),ydermax)
+xlim([Qemin,Qemax])
+
+title('Derivada Característica Estática')
+legend('dh_{X_{VS}min}(q_e)/dq_e','dh_{X_{VS}typ}(q_e)/dq_e', 'dh_{X_{VS}max}(q_e)/dq_e','Location','northeastoutside','Orientation','vertical')
+saveas(gcf,'ej1-dercaracteristicaestatica','epsc')
+
 exit
+
+%
+% Bloque de funciones
+%
+
+% Vector qe
+function qe = qe_vec()
+  rangemin=0;%0.07;
+  rangemax=0.14;
+  steps=10000;
+  qe=rangemin:(rangemax-rangemin)/steps:rangemax;
+end
+
+%% Q_e(h) en terminos de Xvs
+%
+% h=(qe/{xvs*kvs})^2/(pg)
+% =>
+% qe=xvs*kvs*sqrt(p*g*h)
+function y = qe_h(x_vs)
+  [g,Kvs,p] = consts();
+  y = (qe_vec()/(x_vs*Kvs)).^2/(p*g);
+end
+
+%% Q_e(h) evaluado en h
+%
+function qe = qe_h_val(x_vs,h)
+  [g,Kvs,p] = consts();
+  qe=x_vs*Kvs*sqrt(p*g*h);
+end
+
+function qe = qe_der(x_vs)
+  [g,Kvs,p] = consts();
+  qe=(2*qe_vec())/(p*g*(x_vs*Kvs)^2);
+end
+
+function derqe = qe_der_val(x_vs,qe_val)
+  [g,Kvs,p] = consts();
+  derqe=(2*qe_val)/(p*g*(x_vs*Kvs)^2);
+end
+
+function [g,Kvs,p] = consts()
+  g=9.81;
+  Kvs=0.001;
+  p=1475;
+end
