6c87ec50a7ec6622a17157836e60c2dde061755e
Emilio Rojas <emilio93@gmail.com>
2018-01-23 14:36:30 -0600

    tarea 2 lista

--- trabajo/tarea2/ej2.m
+++ trabajo/tarea2/ej2.m
@@ -1,34 +1,50 @@
 
 
 s=tf('s');
-Kp = 900;
-L = 1 / (s * (s^2+s+1) * (s+5)^2 )
-sys = 1 / (L+1)
 
+% Parte a
+Kp=30
+amax=(-Kp^2+54*Kp+360)/(36*Kp)
+a=1
+
+C=Kp*(s+a)/(s+1);
+P=1/(s*(s+2)*(s+3));
+
+L=C*P;
+sys=1/(1+L)
 p=pole(sys)
-z=zero(sys)
+z=zero(sys);
 
-figure
-%pzmap(sys)
-%plot(z,'o')
-%hold on
-plot(p,'x')
+% Parte b
 
-title('Diagrama de polos y ceros')
-%legend('Ceros','Polos', 'Location','northwest')
-ylabel('Eje Imaginario')
-xlabel('Eje Real')
-saveas(gcf, 'img/ej2-1.eps','epsc');
-
-L = 200 / (s * (s^3 + 6*s^2 + 11*s + 6) )
+% I
+s=tf('s')
+Kp = 20
+L = Kp / (s * (s^2+s+1) * (s+5)^2 );
 sys = 1 / (L+1)
-
 p=pole(sys)
-z=zero(sys)
+z=zero(sys);
 
-
-L = 128 / (s * (s^7 + 3*s^6 + 10*s^5 + 24*s^4 + 48*s^3 + 96*s^2 + 128*s + 192) )
+Kp = 19.2736
+L = Kp / (s * (s^2+s+1) * (s+5)^2 );
 sys = 1 / (L+1)
-
 p=pole(sys)
-z=zero(sys)
+z=zero(sys);
+
+Kp = 19
+L = Kp / (s * (s^2+s+1) * (s+5)^2 );
+sys = 1 / (L+1)
+p=pole(sys)
+z=zero(sys);
+
+% II
+L = 200 / (s * (s^3 + 6*s^2 + 11*s + 6) );
+sys = 1 / (L+1)
+p=pole(sys)
+z=zero(sys);
+
+% III
+L = 128 / (s * (s^7 + 3*s^6 + 10*s^5 + 24*s^4 + 48*s^3 + 96*s^2 + 128*s + 192) );
+sys = 1 / (L+1)
+p=pole(sys)
+z=zero(sys);
