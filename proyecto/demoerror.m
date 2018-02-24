8cda812c98631c716f0f0a77b7ec4a879a8f052a
Emilio Rojas <emilio93@gmail.com>
2018-02-24 16:28:44 -0600

    demo de error

---
+++ proyecto/demoerror.m
@@ -0,0 +1,26 @@
+clear 
+clc
+
+s=tf('s');
+
+t=0:1/1000:5;
+
+ps=-2*(-0.25*s+1)/(s*(0.5*s+1));
+
+psm=-2.1*(-0.24*s+1)/(s*(0.4*s+1));
+
+escalon=heaviside(t);
+
+proceso=lsim(ps,escalon,t);
+modelo=lsim(psm,escalon,t);
+error=modelo-proceso;
+errorabs=(modelo-proceso);
+
+iae=trapz(errorabs);
+
+figure('rend','painters','pos',[5 5 400 400])
+title('Error del Modelo')
+hold on
+plot(t,error,'k')
+plot(t,0*t,'k:')
+legend('error','0')
